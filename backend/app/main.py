069de87ed7143d19d8c0b363b36d5fb0d8bdf669
Neetish Tewari <neetish.tewari@gmail.com>
2025-11-25 23:54:12 +0530

    Allow all CORS origins for debugging

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[origin for origin in origins if origin],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
